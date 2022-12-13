d2c5b3f489839e7e86f4f98f9810f85d631348c2
Indomie-glitch <darthaarnav@gmail.com>
2022-12-13 20:58:11 +0700

    testing

--- src/Pages/Deck_page/HomePage.js
+++ src/Pages/Deck_page/HomePage.js
@@ -3,6 +3,8 @@ import { BsCaretLeftFill, BsFillCaretRightFill } from "react-icons/bs";
 import { FaRegSmileBeam, FaRegFrown } from "react-icons/fa";
 import "./HomePage.css";
 import { useState } from "react";
+import { getDatabase, ref, set } from "firebase/database";
+
 
 export default function HomePage({
   quizMode,
@@ -17,6 +19,14 @@ export default function HomePage({
   setDontKnowItCards,
   updateCard,
 }) {
+function writeUserData(userId, name, email, imageUrl) {
+  const db = getDatabase();
+  set(ref(db, "users/" + userId), {
+    username: name,
+    email: email,
+    profile_picture: imageUrl,
+  });
+} 
   console.log(selectedDeck.content, "this is deck content");
   const [theirAnswer, setTheirAnswer] = useState("");
   const [wrongAnswer, setWrongAnswer] = useState(0);
@@ -41,95 +51,97 @@ export default function HomePage({
   };
 
   return (
-    <h1> Hello</h1> >
-    (
-      <div className="home-page">
-        <div className="sidebar-block"></div>
-        {quizMode === false ? (
-          <div>
-            <h1 className="home-page-title">Home Page</h1>
-            <p>Select a deck to get started!</p>
-          </div>
-        ) : (
-          <div>
-            <h1 className="home-page-title">Practice Time!</h1>
-            <h2 className="selected-deck-title">{selectedDeck.data.name}</h2>
-            <h3 className="card-number">{`${questionNumber + 1}/${
-              selectedDeck.content.length
-            }`}</h3>
-            <div className="quiz-section">
-              <div disabled>
-                <BsCaretLeftFill
-                  className="change-question-button"
-                  onClick={decrementQuestionNumber}
-                />
-              </div>
-              <QuizCard
-                selectedDeck={selectedDeck}
-                questionNumber={questionNumber}
-                cardSide={cardSide}
-                setCardSide={setCardSide}
-                theirAnswer={theirAnswer}
-                wrongAnswer={wrongAnswer}
-                setWrongAnswer={setWrongAnswer}
-                updateCard={updateCard}
-              />
-              <BsFillCaretRightFill
+    <div className="home-page">
+      <div className="sidebar-block"></div>
+      {quizMode === false ? (
+        <div>
+          <h1 className="home-page-title">Home Page</h1>
+          <p>Select a deck to get started!</p>
+          <button
+            onClick={() => {
+              writeUserData("123", "arnav", "asdf@asdf.com", "");
+            }}
+          ></button>
+        </div>
+      ) : (
+        <div>
+          <h1 className="home-page-title">Practice Time!</h1>
+          <h2 className="selected-deck-title">{selectedDeck.data.name}</h2>
+          <h3 className="card-number">{`${questionNumber + 1}/${
+            selectedDeck.content.length
+          }`}</h3>
+          <div className="quiz-section">
+            <div disabled>
+              <BsCaretLeftFill
                 className="change-question-button"
-                onClick={incrementQuestionNumber}
+                onClick={decrementQuestionNumber}
               />
             </div>
-            <div className="know-it-button-section">
-              {selectedDeck.content[questionNumber].options &&
-                selectedDeck.content[questionNumber].options
-                  .split(",")
-                  .map((item, index) => {
-                    return (
-                      <button
-                        className="know-it-button"
-                        key={index}
-                        onClick={() => {
-                          setTheirAnswer(item);
-                          setWrongAnswer((prev) => prev + 1);
-                          updateCard([
-                            ...selectedDeck.content,
-                            (selectedDeck.content[questionNumber].wrongAnswer =
-                              wrongAnswer),
-                          ]);
-                        }}
-                      >
-                        {item}
-                      </button>
-                    );
-                  })}
-
-              <button
-                className="know-it-button"
-                onClick={() =>
-                  setKnowItCards([
-                    ...knowItCards,
-                    selectedDeck.content[questionNumber],
-                  ])
-                }
-              >
-                <FaRegSmileBeam />I know it
-              </button>
-              <button
-                className="know-it-button"
-                onClick={() =>
-                  setDontKnowItCards([
-                    ...dontKnowItCards,
-                    selectedDeck.content[questionNumber],
-                  ])
-                }
-              >
-                <FaRegFrown />I don't know it
-              </button>
-            </div>
+            <QuizCard
+              selectedDeck={selectedDeck}
+              questionNumber={questionNumber}
+              cardSide={cardSide}
+              setCardSide={setCardSide}
+              theirAnswer={theirAnswer}
+              wrongAnswer={wrongAnswer}
+              setWrongAnswer={setWrongAnswer}
+              updateCard={updateCard}
+            />
+            <BsFillCaretRightFill
+              className="change-question-button"
+              onClick={incrementQuestionNumber}
+            />
           </div>
-        )}
-      </div>
-    )
+          <div className="know-it-button-section">
+            {selectedDeck.content[questionNumber].options &&
+              selectedDeck.content[questionNumber].options
+                .split(",")
+                .map((item, index) => {
+                  return (
+                    <button
+                      className="know-it-button"
+                      key={index}
+                      onClick={() => {
+                        setTheirAnswer(item);
+                        setWrongAnswer((prev) => prev + 1);
+                        updateCard([
+                          ...selectedDeck.content,
+                          (selectedDeck.content[questionNumber].wrongAnswer =
+                            wrongAnswer),
+                        ]);
+                      }}
+                    >
+                      {item}
+                    </button>
+                  );
+                })}
+
+            <button
+              className="know-it-button"
+              onClick={() =>
+                setKnowItCards([
+                  ...knowItCards,
+                  selectedDeck.content[questionNumber],
+                ])
+              }
+            >
+              <FaRegSmileBeam />I know it
+            </button>
+            <button
+              className="know-it-button"
+              onClick={() =>
+                setDontKnowItCards([
+                  ...dontKnowItCards,
+                  selectedDeck.content[questionNumber],
+                ])
+              }
+            >
+              <FaRegFrown />I don't know it
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
